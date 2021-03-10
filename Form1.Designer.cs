2c87751f3371b82f195dc5a7e433d3828cbbfe97
dravid3x <39882540+dravid3x@users.noreply.github.com>
2021-03-10 18:17:01 +0100

    Codifica funzionante
    Codifica funzionante del testo con salvataggio della nuova immagine in una folder scelta dall'utente

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,13 @@
             this.fDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.testoTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.DecodificaButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // InserisciButton
             // 
-            this.InserisciButton.Location = new System.Drawing.Point(26, 12);
+            this.InserisciButton.Location = new System.Drawing.Point(26, 26);
             this.InserisciButton.Name = "InserisciButton";
             this.InserisciButton.Size = new System.Drawing.Size(117, 64);
             this.InserisciButton.TabIndex = 1;
@@ -64,11 +66,30 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Inserire il testo da camuffare";
             // 
+            // DecodificaButton
+            // 
+            this.DecodificaButton.Location = new System.Drawing.Point(26, 161);
+            this.DecodificaButton.Name = "DecodificaButton";
+            this.DecodificaButton.Size = new System.Drawing.Size(117, 46);
+            this.DecodificaButton.TabIndex = 4;
+            this.DecodificaButton.Text = "Decodifica";
+            this.DecodificaButton.UseVisualStyleBackColor = true;
+            this.DecodificaButton.Click += new System.EventHandler(this.DecodificaButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 232);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(705, 453);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DecodificaButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.testoTextBox);
             this.Controls.Add(this.InserisciButton);
@@ -85,6 +106,8 @@
         private System.Windows.Forms.OpenFileDialog fDialog1;
         private System.Windows.Forms.TextBox testoTextBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DecodificaButton;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Drawing.Imaging;
 
 namespace Steganografia
 {
@@ -21,8 +22,8 @@ namespace Steganografia
 
         private void InserisciButton_Click(object sender, EventArgs e)
         {
-            int offSetPic = 20;
-            Point posIniziale = new Point(159, 12);
+            int offSetPic = 20, offSetLabel = 15;
+            Point posIniziale = new Point(159, 26);
             DialogResult result = fDialog1.ShowDialog();
             PictureBox immagineOriginale = new PictureBox();
             PictureBox nuovaImmagine = new PictureBox();    //Dichiaro la nuova picturebox per l'immagine modificata
@@ -31,34 +32,50 @@ namespace Steganografia
             {
                 immagineOriginale.Load(fDialog1.FileName);  //Carico l'immagine originale nella prima picturebox
                 immagineOriginale.Size = new Size(immagineOriginale.Image.Width, immagineOriginale.Image.Height);   //Imposto la size della prima picturebox in base alle dimensioni dell'imamgine
+                this.Controls.Add(immagineOriginale);   //Aggiungo la prima picturebox all'interno del form
+                if (testoTextBox.Text.Length != 0)   //Controllo se il testo inserito è maggiore di nulla
+                {
+                    Console.WriteLine(testoTextBox.Text);
+                    Console.WriteLine(ConvertiInBinario(testoTextBox.Text));
+                    nuovaImmagine.Location = new Point(immagineOriginale.Location.X + immagineOriginale.Size.Width + offSetPic, immagineOriginale.Location.Y);  //Imposto la posizione in base alla prima picturebox + un offset
+                    nuovaImmagine.Size = immagineOriginale.Size;    //Copio la dimensione della prima picturebox
+                    Image immagineFinale = Stg(immagineOriginale.Image, testoTextBox.Text);     //Salvo l'immagine che verrà modificata nell'immagine finale
+                    FolderBrowserDialog fBDialog = new FolderBrowserDialog();   //Creo una FolderBrowserDialog per selezionare la cartella di destinazione dell'immagine modificata
+                    fBDialog.Description = "Scegliere dove salvare l'immagine modificata";
+                    DialogResult resultSalvataggio = fBDialog.ShowDialog();     //Mostro il dialogo
+                    if (resultSalvataggio == DialogResult.OK)    //Controllo l'integrità della scelta
+                    {
+                        //Salvo l'immagine modificata nella cartella scelta con estensione bmp e nome originale + "_Convertito"
+                        immagineFinale.Save(fBDialog.SelectedPath + "\\" + fDialog1.SafeFileName.Substring(0, fDialog1.SafeFileName.LastIndexOf('.')) + "_Convertito" + ".bmp");
+                    }
+                    nuovaImmagine.Image = immagineFinale;   //Metto l'immagine modificata nel 
+                    this.Controls.Add(nuovaImmagine);       //Aggiungo la seconda picturebox all'interno del form
+                }
+
+                //Inseriscod dei laber per distinguere l'immagine originale dalla modificata
+                Label primaImmagineLabel = new Label();
+                primaImmagineLabel.Text = "Originale";
+                primaImmagineLabel.Location = new Point(immagineOriginale.Location.X, immagineOriginale.Location.Y - offSetLabel);
+                Controls.Add(primaImmagineLabel);
+                Label seondaImmagineLabel = new Label();
+                seondaImmagineLabel.Text = "Modificata";
+                seondaImmagineLabel.Location = new Point(nuovaImmagine.Location.X, nuovaImmagine.Location.Y - offSetLabel);
+                Controls.Add(seondaImmagineLabel);
             }
-            this.Controls.Add(immagineOriginale);   //Aggiungo la prima picturebox all'interno del form
-            if(testoTextBox.Text.Length != 0)   //Controllo se il testo inserito è maggiore di nulla
-            {
-                Console.WriteLine(testoTextBox.Text);
-                Console.WriteLine(ConvertiInBinario(testoTextBox.Text));
-                nuovaImmagine.Location = new Point(immagineOriginale.Location.X + immagineOriginale.Size.Width + offSetPic, immagineOriginale.Location.Y);  //Imposto la posizione in base alla prima picturebox + un offset
-                nuovaImmagine.Size = immagineOriginale.Size;    //Copio la dimensione della prima picturebox
-                Image immagineFinale = stg(immagineOriginale.Image, testoTextBox.Text);     //Salvo l'immagine modificata nell'immagine finale
-                nuovaImmagine.Image = immagineFinale;   //Metto l'immagine modificata nel 
-                this.Controls.Add(nuovaImmagine);       //Aggiungo la seconda picturebox all'interno del form
-            }
-            //Color primoColore = (immagineOriginale.Image as Bitmap).GetPixel(0, 0);
-            //Color secondoColore = (nuovaImmagine.Image as Bitmap).GetPixel(0, 0);
-            //Console.WriteLine("R: " + primoColore.R.ToString() + "\nG: " + primoColore.G.ToString() + "\nB: " + primoColore.B.ToString());
-            //Console.WriteLine("R: " + secondoColore.R.ToString() + "\nG: " + secondoColore.G.ToString() + "\nB: " + secondoColore.B.ToString());
+            else MessageBox.Show("Selezionato file invalido");
+
         }
 
-        private Image stg(Image immagine, String testo)
+        private Image Stg(Image immagine, String testo)
         {
             Bitmap outputBitmap = new Bitmap(immagine.Width, immagine.Height);
 
             //Cicli per copiare i pixel dell'immagine, per aggiungere il messaggio segreto bisognerà modificare la copia del colore corrente
-            int indice = 0;
-            testo = ConvertiInBinario(testo);
-            for(int y = 0; y < immagine.Height; y++)
+            int indice = 0, carattereFine = 00000011;   //Indice per la posizione nella stringa binaria e carattere identificativo della fine
+            testo = ConvertiInBinario(testo) + carattereFine;
+            for (int y = 0; y < immagine.Height; y++)
             {
-                for(int x = 0; x < immagine.Width; x++)
+                for (int x = 0; x < immagine.Width; x++)
                 {
                     Color coloreCorrente = (immagine as Bitmap).GetPixel(x, y);
                     Color nuovoColore = Color.FromArgb(coloreCorrente.A, (indice < testo.Length) ? ((testo[indice++] == 0) ? coloreCorrente.R & 0 : coloreCorrente.R | 1) : coloreCorrente.R, (indice < testo.Length) ? ((testo[indice++] == 0) ? coloreCorrente.G & 0 : coloreCorrente.G | 1) : coloreCorrente.G, (indice < testo.Length) ? ((testo[indice++] == 0) ? coloreCorrente.B & 0 : coloreCorrente.B | 1) : coloreCorrente.B);  //Nuovo colore con i valori modificati
@@ -74,20 +91,36 @@ namespace Steganografia
         private string ConvertiInBinario(string parola)
         {
             string convertita = "";     //Dichiaro la stringa vuota
-            for(int i = 0; i < parola.Length; i++)      //Scorro tutta la stringa
+            for (int i = 0; i < parola.Length; i++)      //Scorro tutta la stringa
             {
                 string binario = Convert.ToString(parola[i], 2);    //Converto il carattere in binario
-                if(binario.Length < 8)  //Se c'erano zeri di fronte devo rimetterli
+                if (binario.Length < 8)  //Se c'erano zeri di fronte devo rimetterli
                 {
                     string nuovoBinario = "";
-                    int nBitMancanti = dimByte - binario.Length;
-                    for (int k = 0; k < nBitMancanti; k++) nuovoBinario += "0";
-                    nuovoBinario += binario;
+                    int nBitMancanti = dimByte - binario.Length;    //Calcolo quanti 0 mancano
+                    for (int k = 0; k < nBitMancanti; k++) nuovoBinario += "0";     //Inserisco gli 0 mancanti
+                    nuovoBinario += binario;    //Aggiungo la parte già ottenuta
                     binario = nuovoBinario;
                 }
-                convertita += binario;
+                convertita += binario;  //Sommo tutte le conversioni
             }
             return convertita;
         }
+
+        private void DecodificaButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+
+            }
+            else MessageBox.Show("Selezionato file invalido");
+        }
     }
 }
+
+
+//Color primoColore = (immagineOriginale.Image as Bitmap).GetPixel(0, 0);
+//Color secondoColore = (nuovaImmagine.Image as Bitmap).GetPixel(0, 0);
+//Console.WriteLine("R: " + primoColore.R.ToString() + "\nG: " + primoColore.G.ToString() + "\nB: " + primoColore.B.ToString());
+//Console.WriteLine("R: " + secondoColore.R.ToString() + "\nG: " + secondoColore.G.ToString() + "\nB: " + secondoColore.B.ToString());
