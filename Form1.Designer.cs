0b9a37bef869b02b4951d63694db20bf3014e728
dravid3x <39882540+dravid3x@users.noreply.github.com>
2021-03-10 23:12:35 +0100

    Finito V1
    Finito con inserimento da textbox e output a textbox.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,11 +34,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.DecodificaButton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.impostazioniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modalitàDiSalvataggioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // InserisciButton
             // 
-            this.InserisciButton.Location = new System.Drawing.Point(26, 26);
+            this.InserisciButton.Location = new System.Drawing.Point(25, 48);
             this.InserisciButton.Name = "InserisciButton";
             this.InserisciButton.Size = new System.Drawing.Size(117, 64);
             this.InserisciButton.TabIndex = 1;
@@ -52,15 +56,16 @@
             // 
             // testoTextBox
             // 
-            this.testoTextBox.Location = new System.Drawing.Point(26, 120);
+            this.testoTextBox.Location = new System.Drawing.Point(25, 142);
+            this.testoTextBox.Multiline = true;
             this.testoTextBox.Name = "testoTextBox";
-            this.testoTextBox.Size = new System.Drawing.Size(117, 20);
+            this.testoTextBox.Size = new System.Drawing.Size(117, 35);
             this.testoTextBox.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 104);
+            this.label1.Location = new System.Drawing.Point(13, 126);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(139, 13);
             this.label1.TabIndex = 3;
@@ -68,7 +73,7 @@
             // 
             // DecodificaButton
             // 
-            this.DecodificaButton.Location = new System.Drawing.Point(26, 161);
+            this.DecodificaButton.Location = new System.Drawing.Point(25, 192);
             this.DecodificaButton.Name = "DecodificaButton";
             this.DecodificaButton.Size = new System.Drawing.Size(117, 46);
             this.DecodificaButton.TabIndex = 4;
@@ -78,24 +83,60 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(26, 232);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(25, 254);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.Size = new System.Drawing.Size(117, 112);
             this.textBox1.TabIndex = 5;
+            // 
+            // impostazioniToolStripMenuItem
+            // 
+            this.impostazioniToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modalitàDiSalvataggioToolStripMenuItem});
+            this.impostazioniToolStripMenuItem.Name = "impostazioniToolStripMenuItem";
+            this.impostazioniToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.impostazioniToolStripMenuItem.Text = "Impostazioni";
+            // 
+            // modalitàDiSalvataggioToolStripMenuItem
+            // 
+            this.modalitàDiSalvataggioToolStripMenuItem.Name = "modalitàDiSalvataggioToolStripMenuItem";
+            this.modalitàDiSalvataggioToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.modalitàDiSalvataggioToolStripMenuItem.Text = "Modalità di salvataggio";
+            this.modalitàDiSalvataggioToolStripMenuItem.ToolTipText = "Salva solo pixel modificati o intera immagine, default intera";
+            this.modalitàDiSalvataggioToolStripMenuItem.Click += new System.EventHandler(this.modalitàDiSalvataggioToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.impostazioniToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(705, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(705, 453);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(705, 378);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.DecodificaButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.testoTextBox);
             this.Controls.Add(this.InserisciButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,6 +149,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button DecodificaButton;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem impostazioniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modalitàDiSalvataggioToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,11 @@ namespace Steganografia
 {
     public partial class Form1 : Form
     {
-        const int dimByte = 8;
+        const int dimByte = 8, offSetLabel = 15;
+        bool visualizzaSoloMex = false, prima = true;
+        PictureBox immagineOriginale = new PictureBox();
+        PictureBox nuovaImmagine = new PictureBox();    //Dichiaro la nuova picturebox per l'immagine modificata
+
         public Form1()
         {
             InitializeComponent();
@@ -22,12 +26,16 @@ namespace Steganografia
 
         private void InserisciButton_Click(object sender, EventArgs e)
         {
-            int offSetPic = 20, offSetLabel = 15;
-            Point posIniziale = new Point(159, 26);
+            //Controllo la lunghezza del testo inserito, ovvero se l'utente ha inserito del testo
+            if (testoTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Inserire prima il testo da camuffare");
+                return;
+            }
+            int offSetPic = 20;
             DialogResult result = fDialog1.ShowDialog();
-            PictureBox immagineOriginale = new PictureBox();
-            PictureBox nuovaImmagine = new PictureBox();    //Dichiaro la nuova picturebox per l'immagine modificata
-            immagineOriginale.Location = posIniziale;       //Imposto la posizione della prima picturebox
+
+            immagineOriginale.Location = new Point((InserisciButton.Location.X * 2) + InserisciButton.Size.Width, InserisciButton.Location.Y);       //Imposto la posizione della prima picturebox
             if (result == DialogResult.OK)  //Controllo se è stato selezionato un file adatto
             {
                 immagineOriginale.Load(fDialog1.FileName);  //Carico l'immagine originale nella prima picturebox
@@ -35,8 +43,8 @@ namespace Steganografia
                 this.Controls.Add(immagineOriginale);   //Aggiungo la prima picturebox all'interno del form
                 if (testoTextBox.Text.Length != 0)   //Controllo se il testo inserito è maggiore di nulla
                 {
-                    Console.WriteLine(testoTextBox.Text);
-                    Console.WriteLine(ConvertiInBinario(testoTextBox.Text));
+                    //Console.WriteLine(testoTextBox.Text);
+                    //Console.WriteLine(ConvertiInBinario(testoTextBox.Text));
                     nuovaImmagine.Location = new Point(immagineOriginale.Location.X + immagineOriginale.Size.Width + offSetPic, immagineOriginale.Location.Y);  //Imposto la posizione in base alla prima picturebox + un offset
                     nuovaImmagine.Size = immagineOriginale.Size;    //Copio la dimensione della prima picturebox
                     Image immagineFinale = Stg(immagineOriginale.Image, testoTextBox.Text);     //Salvo l'immagine che verrà modificata nell'immagine finale
@@ -46,21 +54,30 @@ namespace Steganografia
                     if (resultSalvataggio == DialogResult.OK)    //Controllo l'integrità della scelta
                     {
                         //Salvo l'immagine modificata nella cartella scelta con estensione bmp e nome originale + "_Convertito"
-                        immagineFinale.Save(fBDialog.SelectedPath + "\\" + fDialog1.SafeFileName.Substring(0, fDialog1.SafeFileName.LastIndexOf('.')) + "_Convertito" + ".bmp");
+                        try
+                        {
+                            immagineFinale.Save(fBDialog.SelectedPath + "\\" + fDialog1.SafeFileName.Substring(0, fDialog1.SafeFileName.LastIndexOf('.')) + "_Convertito" + ".bmp");
+                        }
+                        catch (Exception errore)
+                        {
+                            MessageBox.Show("Qualcosa è andato storto\n" + errore.Message);
+                        }
                     }
                     nuovaImmagine.Image = immagineFinale;   //Metto l'immagine modificata nel 
                     this.Controls.Add(nuovaImmagine);       //Aggiungo la seconda picturebox all'interno del form
                 }
-
-                //Inseriscod dei laber per distinguere l'immagine originale dalla modificata
+                //Inseriscod dei label per distinguere l'immagine originale dalla modificata. Se eseguo più volte la conversione i label si overlappano
                 Label primaImmagineLabel = new Label();
+                primaImmagineLabel = new Label();
                 primaImmagineLabel.Text = "Originale";
                 primaImmagineLabel.Location = new Point(immagineOriginale.Location.X, immagineOriginale.Location.Y - offSetLabel);
                 Controls.Add(primaImmagineLabel);
                 Label seondaImmagineLabel = new Label();
+                seondaImmagineLabel = new Label();
                 seondaImmagineLabel.Text = "Modificata";
                 seondaImmagineLabel.Location = new Point(nuovaImmagine.Location.X, nuovaImmagine.Location.Y - offSetLabel);
                 Controls.Add(seondaImmagineLabel);
+
             }
             else MessageBox.Show("Selezionato file invalido");
 
@@ -71,21 +88,68 @@ namespace Steganografia
             Bitmap outputBitmap = new Bitmap(immagine.Width, immagine.Height);
 
             //Cicli per copiare i pixel dell'immagine, per aggiungere il messaggio segreto bisognerà modificare la copia del colore corrente
-            int indice = 0, carattereFine = 00000011;   //Indice per la posizione nella stringa binaria e carattere identificativo della fine
-            testo = ConvertiInBinario(testo) + carattereFine;
-            for (int y = 0; y < immagine.Height; y++)
+            int indice = 0;//Indice per la posizione nella stringa binaria
+            string carattereFine = "00000100";    //carattere identificativo della fine del testo
+            testo = ConvertiInBinario(testo);
+            testo += carattereFine;
+            //Console.WriteLine(testo);
+            int y = 0;
+            bool finitoTesto = false;
+            while (y < immagine.Height)
             {
-                for (int x = 0; x < immagine.Width; x++)
+                int x = 0;
+                while (x < immagine.Width)
                 {
                     Color coloreCorrente = (immagine as Bitmap).GetPixel(x, y);
-                    Color nuovoColore = Color.FromArgb(coloreCorrente.A, (indice < testo.Length) ? ((testo[indice++] == 0) ? coloreCorrente.R & 0 : coloreCorrente.R | 1) : coloreCorrente.R, (indice < testo.Length) ? ((testo[indice++] == 0) ? coloreCorrente.G & 0 : coloreCorrente.G | 1) : coloreCorrente.G, (indice < testo.Length) ? ((testo[indice++] == 0) ? coloreCorrente.B & 0 : coloreCorrente.B | 1) : coloreCorrente.B);  //Nuovo colore con i valori modificati
-                    outputBitmap.SetPixel(x, y, nuovoColore);
+                    Color nuovoColore;
+                    int R = coloreCorrente.R, G = coloreCorrente.G, B = coloreCorrente.B;
+                    if (!finitoTesto)
+                    {
+                        /*In questi controlli verifico se ho terminato di "criptare" tutto il testo inserito, se no converto il valore del colore R o G o B (1 per volta) in binario all'interno di una stringa
+                         *Successivamente converto la stringa in array di tipo char per poter manipolare i singoli caratteri, ovvero i singoli bit del valore, imposterò poi l'ultimo bit del valore attuale a 0 o 1
+                         *Passo al valore successivo sia nella sequenza RGB sia nella posizione all'interno del testo in binario. Quando terminerò il testo o copierò i pixel così come sono o non li copierò affatto in base a visualizzaSoloMex
+                         */
+                        if (indice < testo.Length)
+                        {
+                            string temp = Convert.ToString(coloreCorrente.R, 2);
+                            char[] char_temp = temp.ToCharArray();
+                            char_temp[char_temp.Length - 1] = (testo[indice++] == '0') ? '0' : '1';
+                            temp = new string(char_temp);
+                            R = Convert.ToInt32(temp, 2);
+                        }
+                        else finitoTesto = true;
+                        if (indice < testo.Length)
+                        {
+                            string temp = Convert.ToString(coloreCorrente.G, 2);
+                            char[] char_temp = temp.ToCharArray();
+                            char_temp[char_temp.Length - 1] = (testo[indice++] == '0') ? '0' : '1';
+                            temp = new string(char_temp);
+                            G = Convert.ToInt32(temp, 2);
+                        }
+                        else finitoTesto = true;
+                        if (indice < testo.Length)
+                        {
+                            string temp = Convert.ToString(coloreCorrente.B, 2);
+                            char[] char_temp = temp.ToCharArray();
+                            char_temp[char_temp.Length - 1] = (testo[indice++] == '0') ? '0' : '1';
+                            temp = new string(char_temp);
+                            B = Convert.ToInt32(temp, 2);
+                        }
+                        else finitoTesto = true;
+                        nuovoColore = Color.FromArgb(coloreCorrente.A, R, G, B);
+                    }
+                    else nuovoColore = coloreCorrente;
+                    //Opzione per mostrare solo i pixel del testo o tutta l'immagine converita.
+                    if (visualizzaSoloMex)
+                    {
+                        if (!finitoTesto) outputBitmap.SetPixel(x, y, nuovoColore); //Imposto il pixel della nuova immagine in base ai valori calcolati
+                    }
+                    else outputBitmap.SetPixel(x, y, nuovoColore);  //Imposto il pixel della nuova immagine in base ai valori calcolati
+                    x++;
                 }
+                y++;
             }
-
-            //Console.WriteLine("R: " + coloreCorrente.R.ToString() + "\nG: " + coloreCorrente.G.ToString() + "\nB: " + coloreCorrente.B.ToString());
-
-            return outputBitmap;
+            return outputBitmap;    //Restituisco l'immagine modificata
         }
 
         private string ConvertiInBinario(string parola)
@@ -109,18 +173,93 @@ namespace Steganografia
 
         private void DecodificaButton_Click(object sender, EventArgs e)
         {
+            textBox1.ScrollBars = ScrollBars.Vertical;
             DialogResult result = fDialog1.ShowDialog();
             if (result == DialogResult.OK)
             {
-
+                //Dichiaro e aggiungo una picturebox contenente l'immagine selezionata
+                PictureBox immagineOriginale = new PictureBox();
+                immagineOriginale.Location = new Point((InserisciButton.Location.X * 2) + InserisciButton.Size.Width, InserisciButton.Location.Y);
+                immagineOriginale.Load(fDialog1.FileName);  //Carico l'immagine originale nella prima picturebox
+                immagineOriginale.Size = new Size(immagineOriginale.Image.Width, immagineOriginale.Image.Height);   //Imposto la size della prima picturebox in base alle dimensioni dell'
+                string testoRicavato = Desteganografatore(immagineOriginale.Image);     //Richiamo la funzione di decodifica, potevo chiamarlo decodifica ma non puù dire che Desteganografatore non è fantastico come nome
+                //Console.WriteLine(testoRicavato);
+                textBox1.Text = testoRicavato;
             }
             else MessageBox.Show("Selezionato file invalido");
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            testoTextBox.ScrollBars = ScrollBars.Vertical;  //Aggiungo la scrollbar verticale
+        }
+
+        private void modalitàDiSalvataggioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            visualizzaSoloMex = !visualizzaSoloMex; //Abilito o disabilito la modalità solo mex
+            MessageBox.Show("Ora verranno salvati " + ((visualizzaSoloMex) ? "solamente i pixel del messaggio." : "tutti i pixel dell'immagine"));
+        }
+
+        private string Desteganografatore(Image immagine)
+        {
+            int dim = immagine.Width * immagine.Height, x = 0, y = 0, posColore = 0;
+            string carattereFine = "00000100";
+            bool tailDetected = false;
+            string testoFinale = "";
+            while (!tailDetected)
+            {
+                int pos = 0;
+                string carattereAttuale = "";
+                while (pos < dimByte)
+                {
+                    Color colore = (immagine as Bitmap).GetPixel(x, y);
+                    //Switch ce esegue la conversione e salvataggio del char trovato in base al turno R, G o B
+                    switch (posColore)
+                    {
+                        case 0:
+                            {
+                                //Converto in binario, poi in array di char e prendo il LSB
+                                string temp = Convert.ToString(colore.R, 2);
+                                char[] char_temp = temp.ToCharArray();
+                                carattereAttuale += (char_temp[char_temp.Length - 1] == '0') ? "0" : "1";
+                            }
+                            break;
+                        case 1:
+                            {
+                                //Converto in binario, poi in array di char e prendo il LSB
+                                string temp = Convert.ToString(colore.G, 2);
+                                char[] char_temp = temp.ToCharArray();
+                                carattereAttuale += (char_temp[char_temp.Length - 1] == '0') ? "0" : "1";
+                            }
+                            break;
+                        case 2:
+                            {
+                                //Converto in binario, poi in array di char e prendo il LSB
+                                string temp = Convert.ToString(colore.B, 2);
+                                char[] char_temp = temp.ToCharArray();
+                                carattereAttuale += (char_temp[char_temp.Length - 1] == '0') ? "0" : "1";
+                            }
+                            break;
+                    }
+                    //Controllo per ricominciare da R se arrivo a B, con annesso controllo della x per passare al pixel successivo senza sbordare, se sbordo aumento la y e resetto la x
+                    if (posColore == 2)
+                    {
+                        posColore = 0;
+                        if (x + 1 < immagine.Width) x++;
+                        else
+                        {
+                            x = 0;
+                            y++;
+                        }
+                    }
+                    else posColore++;
+                    pos++;
+                }
+                char carattereFinale = (char)Convert.ToInt32(carattereAttuale, 2);
+                if (carattereFinale == (char)Convert.ToInt32(carattereFine, 2)) tailDetected = true;    //Controllo per identificare la tail che indica la fine del messaggio
+                else testoFinale += carattereFinale;
+            }
+            return testoFinale;
+        }
     }
 }
-
-
-//Color primoColore = (immagineOriginale.Image as Bitmap).GetPixel(0, 0);
-//Color secondoColore = (nuovaImmagine.Image as Bitmap).GetPixel(0, 0);
-//Console.WriteLine("R: " + primoColore.R.ToString() + "\nG: " + primoColore.G.ToString() + "\nB: " + primoColore.B.ToString());
-//Console.WriteLine("R: " + secondoColore.R.ToString() + "\nG: " + secondoColore.G.ToString() + "\nB: " + secondoColore.B.ToString());
